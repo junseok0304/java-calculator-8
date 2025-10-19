17eb95b3967908d08fb31ccacc3efa3a92df10af
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-10-19 19:13:13 +0900

    fix: 예제 테스트 실행 문제 해결
    try-catch로 return하는 구문 삭제, 문제에서 제시한 조건에 맞도록 수정
    
    - try-catch 구문 삭제

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -6,15 +6,11 @@ import calculator.view.OutputView;
 
 public class CalculatorController {
     public void run() {
-        try {
             String input = InputView.readInput();
 
             StringCalculator calculator = new StringCalculator();
             int result = calculator.add(input);
 
             OutputView.print(result);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
